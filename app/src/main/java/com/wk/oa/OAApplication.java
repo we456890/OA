072351748da1939f21c3c5a4560234ed46bd1557
we456890 <785601918@qq.com>
2017-08-07 18:48:11 +0800

    Application初始化完成

--- app/src/main/java/com/wk/oa/OAApplication.java
+++ app/src/main/java/com/wk/oa/OAApplication.java
@@ -1,8 +1,23 @@
 package com.wk.oa;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Environment;
+import android.text.TextUtils;
+
+import com.netease.nim.uikit.NimUIKit;
+import com.netease.nim.uikit.session.viewholder.MsgViewHolderThumbBase;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.SDKOptions;
+import com.netease.nimlib.sdk.StatusBarNotificationConfig;
+import com.netease.nimlib.sdk.auth.LoginInfo;
+import com.wk.oa.activity.MainActivity;
+import com.wk.oa.bean.UserDTO;
+import com.wk.oa.utils.AcacheUtils;
+import com.wk.oa.utils.SystemUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +27,7 @@ import java.util.List;
  */
 
 public class OAApplication extends Application {
+    private static final String TAG = "OAApplication";
     public static Context applicationContext;
     private static OAApplication instance;
 
@@ -28,6 +44,110 @@ public class OAApplication extends Application {
         super.onCreate();
         applicationContext = this;
         instance = this;
+
+        // SDK初始化（启动后台服务，若已经存在用户登录信息， SDK 将完成自动登录）
+
+
+        if (inMainProcess(this)) {
+            // 注意：以下操作必须在主进程中进行
+            // 1、UI相关初始化操作
+            // 2、相关Service调用
+            NIMClient.init(this, loginInfo(), options());
+            // 初始化UIKit模块
+            initUIKit();
+
+        }
+    }
+
+    private void initUIKit() {
+        // 初始化，使用 uikit 默认的用户信息提供者
+        NimUIKit.init(this);
+
+    }
+
+    public static boolean inMainProcess(Context context) {
+        String packageName = context.getPackageName();
+        String processName = SystemUtil.getProcessName(context);
+        return packageName.equals(processName);
+    }
+
+    // 一定不能为null，否则初始化NIMClient.init空指针
+    private SDKOptions options() {
+        SDKOptions options = new SDKOptions();
+
+        // 如果将新消息通知提醒托管给 SDK 完成，需要添加以下配置。否则无需设置。
+        StatusBarNotificationConfig config = new StatusBarNotificationConfig();
+        config.notificationEntrance = MainActivity.class; // 点击通知栏跳转到该Activity
+        config.notificationSmallIconId = R.mipmap.ic_launcher;
+        // 呼吸灯配置
+        config.ledARGB = Color.GREEN;
+        config.ledOnMs = 1000;
+        config.ledOffMs = 1500;
+        // 通知铃声的uri字符串
+        config.notificationSound = "android.resource://com.netease.nim.demo/raw/msg";
+        options.statusBarNotificationConfig = config;
+
+        // 配置保存图片，文件，log 等数据的目录
+        // 如果 options 中没有设置这个值，SDK 会使用下面代码示例中的位置作为 SDK 的数据目录。
+        // 该目录目前包含 log, file, image, audio, video, thumb 这6个目录。
+        // 如果第三方 APP 需要缓存清理功能， 清理这个目录下面个子目录的内容即可。
+        String sdkPath = Environment.getExternalStorageDirectory() + "/" + getPackageName() + "/nim";
+        options.sdkStorageRootPath = sdkPath;
+
+        // 配置是否需要预下载附件缩略图，默认为 true
+        options.preloadAttach = true;
+
+        // 配置附件缩略图的尺寸大小。表示向服务器请求缩略图文件的大小
+        // 该值一般应根据屏幕尺寸来确定， 默认值为 Screen.width / 2
+        options.thumbnailSize = MsgViewHolderThumbBase.getImageMaxEdge();
+
+        return options;
+    }
+
+    private LoginInfo loginInfo() {
+        AcacheUtils mAcache = AcacheUtils.get(OAApplication.getContext());
+        UserDTO uInfo = (UserDTO) mAcache.getAsObject("user");
+        String account = uInfo.getId();
+        String token = uInfo.getToken();
+        if (!TextUtils.isEmpty(account) && !TextUtils.isEmpty(token)) {
+            return new LoginInfo(account, token);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取当前进程名
+     *
+     * @param context
+     * @return 进程名
+     */
+    public static final String getProcessName(Context context) {
+        String processName = null;
+
+        // ActivityManager
+        ActivityManager am = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE));
+
+        while (true) {
+            for (ActivityManager.RunningAppProcessInfo info : am.getRunningAppProcesses()) {
+                if (info.pid == android.os.Process.myPid()) {
+                    processName = info.processName;
+                    break;
+                }
+            }
+
+            // go home
+            if (!TextUtils.isEmpty(processName)) {
+                return processName;
+            }
+
+            // take a rest and again
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     //用于存放我们所有activity的数组
